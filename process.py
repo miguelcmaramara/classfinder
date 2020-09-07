36132f452c8867cd034aeed564875ab0b2a1e54d
Miguel Maramara <miguelcmaramara@gmail.com>
2020-09-07 11:22:57 -0400

    Finished processsing code

--- process.py
+++ process.py
@@ -10,6 +10,42 @@ with open("classfinder/spiders/raw_courses.json") as file:
 
 # pp.pprint(data)
 
+def reqParse(reqs):
+    # How to structure prereq:
+    # "," indicates "or", ";" indicated "and"
+    # a & b -> a;b -> [[a][b]]
+    # a or b -> a,b -> [[a, b]]
+    # no need for testing, usually we end up with long lists
+    reqs = re.sub(r"[,|\s]?\Wand\W", r";", reqs)            # and with ;
+    reqs = re.sub(r"[,|\s]?\Wor\W", r",", reqs)             # or with ,
+    reqs = re.sub(r"\s", r"", reqs)                         # \s with ""
+    reqs = re.sub(r"([A-Z]{3})(\d{3}.)(\d{3})", r"\1\2\1\3", reqs)     # or with ,
+    reqs = re.sub(r"([A-Z]{3})(\d{3}.)(\d{3})", r"\1\2\1\3", reqs)     # repeat to catch multiple cases
+    reqs = re.sub(r"([A-Z]{3})(\d{3}.)(\d{3})", r"\1\2\1\3", reqs)     # repeat to catch multiple cases
+    
+    reqs = re.sub(r"([;,]?)[^,;]*[pP]lacement[^,;]*([;,]?)", r"\1Placement\2", reqs)              # changes placement
+    reqs = re.sub(r"([,;]?)(U\d)[^,;]*([,;]?)", r"\1\2\3", reqs)            # U1-U4
+    reqs = re.sub(r"([,;]?)[^,;]*[gG].?[pP].?[aA][^,;]*([,;]?)", r"\1GPA\2", reqs)            # gpa
+    reqs = re.sub(r"([,;]?)[^,;]*[dD].?[eE].?[cC][^,;]*([,;]?)", r"", reqs)            # DEC
+    reqs = re.sub(r"([,;]?)[^,;]*([A-Z]{3}\d{3})[^,;]*([,;]?)", r"\1\2\3", reqs)            # cleans AAA000 classformat
+    reqs = re.sub(r"[;,]?[pP]ermission[^,;]*[;,]?", r"", reqs)              # Gets rid of 'permission'
+    reqs = re.sub(r"[;,]?[^,;]*[gG]rade[^,;]*[;,]?", r"", reqs)              # Gets rid of 'grades'
+    reqs = re.sub(r"[;,]?[^,;]*[lL]evel[^,;]*[;,]?", r"", reqs)              # Gets rid of 'level'
+    # reqs = re.sub(r"([,;]?)[^,;]*[,;]?[^,;]*betterin([A-Z]{3}\d{3})([,;]?)", r"\1\2\3", reqs)            # "betterin
+
+    results = []    # turns long string into lists, filtering out long and short
+    result = []
+    for req in reqs.split(";"):
+        for subreqs in req.split(","):
+            if (len(subreqs) >= 2 and len(subreqs) <= 10):
+                result.append(subreqs)
+        results.append(result)
+        results = list(filter(None, results))
+        result = []
+            
+    return results
+
+
 courses = {}
 #   Structure --------------------------
 #   courses:
@@ -18,7 +54,7 @@ courses = {}
 #           >name : "course of courses!"
 #           >number : ###
 #           >desc : 
-#           SBC 
+#           >SBC 
 #           >coreq
 #           >prereq
 #           >credits
@@ -36,6 +72,8 @@ reOCredits = r"\1"
 reICoReq = r".*[Cc]o-?requisites?:?\s?(.*)"
 
 reIPreReq = r".*Prerequisites?:?\s?(.*)"
+
+# REGEX post Processing ---------------------
 # Loop to enact
 for rawCourse in data:
     courseId = re.sub(reIClass,reOClass,rawCourse["className"][0])
@@ -47,16 +85,20 @@ for rawCourse in data:
     except:
         courseCredits = "Unavailable"
 
+    # checks length -- as SBC name lengths are between 3 and 4 inclusively
     # Use find to find the "corequisite"/"prerequisite" word -> if the classes exist thereafter,
     # it processes it. If not, it waits for the next line
     courseCoReq = ''
     coursePreReq = ''
+    sbc = []
     nlco = False
     nlpre = False
-    print(courseId)
     for extra in rawCourse["extras"]:    #Coreq
         courseCoReqArr=re.findall(reICoReq, extra)
         coursePreReqArr=re.findall(reIPreReq, extra)
+
+        if(len(extra) <=4 and len(extra) >= 3):
+            sbc.append(extra)
 
         if nlco:      # See if the REGEX was caught, but the classes were not in the same line
             courseCoReq = extra
@@ -77,9 +119,9 @@ for rawCourse in data:
                 nlpre = True
         except:
             pass
+    coursePreReq = reqParse(coursePreReq)
+    courseCoReq = reqParse(courseCoReq)
 
-    
-    
     courses[courseId] = {
             "id" : courseId,
             "number" : int(courseNumber),
@@ -88,14 +130,31 @@ for rawCourse in data:
             "desc" : rawCourse["desc"][0],
             "coReq" : courseCoReq,
             "preReq" : coursePreReq,
-            "SBC" : sbc
+            "SBC" : sbc,
             }
+
+
 
 # pp.pprint(courses)
 for name, course in courses.items():
     print("------------------")
     print(name)
+    print("number")
+    print(course["number"])
+    print("id")
+    print(course["id"])
+    print("desc")
+    print(course["desc"])
+    print("coReq")
     print(course["coReq"])
+    print("preReq")
     print(course["preReq"])
+    print("SBC")
+    print(course["SBC"])
+    print("credits")
+    print(course["credits"])
+out_file = open("myfile.json", "w") 
 
+json.dump(courses,out_file,indent =4)
+out_file.close()
 
